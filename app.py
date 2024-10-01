2a6f9b32c233acc11dcd95b1fd170d0b80d6dc7c
Thomas <tj.collins1117@gmail.com>
2024-10-01 10:50:13 -0700

    force apply migrations

--- app.py
+++ app.py
@@ -1,14 +1,3 @@
-# from flask import Flask
-# from routes.user_routes import user_routes
-# from routes.product_routes import product_routes
-# from routes.order_routes import order_routes
-
-# app = Flask(__name__)
-
-# app.register_blueprint(user_routes, url_prefix='/api')
-# app.register_blueprint(product_routes, url_prefix='/api')
-# app.register_blueprint(order_routes, url_prefix='/api')
-
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
@@ -18,36 +7,44 @@ from routes.review import review_bp
 from routes.twitch_api import twitch_bp
 from routes.user import user_bp
 from models import db, Game, Users, Review, ReviewInfo  # Import the db object
-from flask_migrate import Migrate
+from flask_migrate import Migrate, upgrade
 
 app = Flask(__name__)
 
 CORS(app, resources={r"/api/*": {"origins": "*"}})  # Allow all origins for API routes
 
-
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:ArkhamknightN7?@localhost:5432/gamespective'  # Update with your database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  # Update with your database URI
+# Use the DATABASE_URL environment variable
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')  
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db.init_app(app)  # Initialize the db object with the app
-migrate = Migrate(app, db)  # Initialize Flask-Migrate
 
-with app.app_context():
-    db.create_all()  # Create the tables
+# Initialize the db object with the app
+db.init_app(app)  
 
+# Initialize Flask-Migrate
+migrate = Migrate(app, db)  
+
+def apply_migrations():
+    with app.app_context():
+        try:
+            # Run this to apply any pending migrations
+            upgrade()  
+            print("Migrations applied successfully.")
+        except Exception as e:
+            print(f"Error applying migrations: {e}")
+
+# Apply migrations at startup
+apply_migrations()
+
+# Register your blueprints
 app.register_blueprint(auth_bp, url_prefix='/api/authRoutes')
 app.register_blueprint(game_bp, url_prefix='/api/games')
 app.register_blueprint(review_bp, url_prefix='/api/review')
 app.register_blueprint(twitch_bp, url_prefix='/api/twitch_api')
 app.register_blueprint(user_bp, url_prefix='/api/user')
 
-
-
-
 @app.route('/')
 def home():
     return "Hello, Flask!"
-# @app.route('/api/data', methods=['GET'])
-# def get_date():
-#     return jsonify({"message": "Hello from Flask!"})
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
